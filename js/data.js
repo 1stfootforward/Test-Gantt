7bc37ac8597bf242052e5277557680ce2111fd42
olijames <olimjames@gmail.com>
2016-01-27 14:08:17 +1300

    removed date bugs

--- js/data.js
+++ js/data.js
@@ -1,20 +1,20 @@
 var task = {
     "0": {"sdate":"January 26, 2016", "edate":"January 27, 2016", "duration":null, "name":"Task Name", "category":"Company One", "depends":null},
     "1": {"sdate":"January 28, 2016", "edate":"January 30, 2016", "duration":null, "name":"Task Name 2", "category":"Company One", "depends":null},
-    "2": {"sdate":"January 31, 2016", "edate":"Febuary 1, 2016", "duration":null, "name":"Task Name 3", "category":"Company One", "depends":"1"},
+    "2": {"sdate":"January 31, 2016", "edate":"Febuary 1, 2016", "duration":null, "name":"Task Name 3", "category":"Company One", "depends":null},
     "3": {"sdate":"Febuary 6, 2016", "edate":"Febuary 11, 2016", "duration":null, "name":"Task Name 4", "category":"Company One", "depends":null},
-    "4": {"sdate":"January 2, 2016", "edate":"January 11, 2016", "duration":null, "name":"Task Name 5", "category":"Company Two", "depends":null},
-    "5": {"sdate":"January 12, 2016", "edate":"January 16, 2016", "duration":null, "name":"Task Name 6", "category":"Company Two", "depends":null},
+    "4": {"sdate":"January 11, 2016", "edate":"January 13, 2016", "duration":null, "name":"Task Name 5", "category":"Company Two", "depends":null},
+    "5": {"sdate":"January 16, 2016", "edate":"January 19, 2016", "duration":null, "name":"Task Name 6", "category":"Company Two", "depends":null},
     "6": {"sdate":"January 25, 2016", "edate":"January 27, 2016", "duration":null, "name":"Task Name 7", "category":"Company Three", "depends":null},
     "7": {"sdate":"January 31, 2016", "edate":"Febuary 3, 2016", "duration":null, "name":"Task Name 8", "category":"Company Three", "depends":null},
     "8": {"sdate":"Febuary 11, 2016", "edate":"Febuary 18, 2016", "duration":null, "name":"Task Name 9", "category":"Company Three", "depends":null},
-    "9": {"sdate":"Febuary 13, 2016", "edate":"Febuary 17, 2016", "duration":null, "name":"Task Name 10", "category":"Company Four", "depends":"5"},
+    "9": {"sdate":"Febuary 13, 2016", "edate":"Febuary 17, 2016", "duration":null, "name":"Task Name 10", "category":"Company Four", "depends":null},
     "10": {"sdate":"Febuary 22, 2016", "edate":"Febuary 29, 2016", "duration":null, "name":"Task Name 11", "category":"Company Four", "depends":null},
     "11": {"sdate":"Febuary 25, 2016", "edate":"Febuary 31, 2016", "duration":null, "name":"Task Name 12", "category":"Company Four", "depends":null},
     "12": {"sdate":"January 6, 2016", "edate":"January 15, 2016", "duration":null, "name":"Task Name 13", "category":"Company Five", "depends":null},
     "13": {"sdate":"Febuary 1, 2016", "edate":"Febuary 13, 2016", "duration":null, "name":"Task Name 14", "category":"Company Five", "depends":null},
-    "14": {"sdate":"January 13, 2016", "edate":"January 15, 2016", "duration":null, "name":"Task Name 15", "category":"Company Six", "depends":null},
-    "15": {"sdate":"January 17, 2016", "edate":"January 19, 2016", "duration":null, "name":"Task Name 16", "category":"Company Six", "depends":"14"},
+    "14": {"sdate":"January 24, 2016", "edate":"January 25, 2016", "duration":null, "name":"Task Name 15", "category":"Company Six", "depends":null},
+    "15": {"sdate":"January 26, 2016", "edate":"January 27, 2016", "duration":null, "name":"Task Name 16", "category":"Company Seven", "depends":"14"}
 };
 
 var active;
