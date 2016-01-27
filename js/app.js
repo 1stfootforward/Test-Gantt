6f36b785aa619784f494f78b53a1afe944733c4b
olijames <olimjames@gmail.com>
2016-01-28 12:43:28 +1300

    new style

---
+++ js/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/* App Module */
+
+var app = angular.module('ganttApp', [ ]);
+
+app.controller('MainGanttCtrl', function($scope) {
+
+  $scope.tasks = {
+    data:[
+      {id:1, text:"Company ", start_date:"25-01-2016", duration:6, order:10, open: true, color:"blue"},
+      {id:2, text:"Task ",    start_date:"25-01-2016", duration:2, order:10, parent:1, color:"blue"},
+      {id:3, text:"Task ",    start_date:"28-01-2016", duration:3, order:10, parent:1, color:"blue"},
+      {id:4, text:"Company ", start_date:"25-01-2016", duration:4,order:10, open: false, color:"red"},
+      {id:5, text:"Task ",    start_date:"25-01-2016", duration:2, order:10, parent:4, color:"red"},
+      {id:6, text:"Task ",    start_date:"26-01-2016", duration:3, order:10, parent:4, color:"red"},
+      {id:7, text:"Company ", start_date:"25-01-2016", duration:8,order:10, open: false, color:"orange"},
+      {id:8, text:"Task ",    start_date:"25-01-2016", duration:2, order:10, parent:7, color:"orange"},
+      {id:9, text:"Task ",    start_date:"28-01-2016", duration:4, order:10, parent:7, color:"orange"},
+      {id:10, text:"Company ", start_date:"22-01-2016", duration:7,order:10, open: false, color:"green"},
+      {id:11, text:"Task ",    start_date:"22-01-2016", duration:2, order:10, parent:10, color:"green"},
+      {id:12, text:"Task ",    start_date:"22-01-2016", duration:7, order:10, parent:10, color:"green"},
+      {id:13, text:"Company ", start_date:"1-02-2016", duration:5, order:10, open: false, color:"purple"},
+      {id:14, text:"Task ",    start_date:"1-02-2016", duration:2, order:10, parent:13, color:"purple"},
+      {id:15, text:"Task ",    start_date:"3-02-2016", duration:3, order:10, parent:13, color:"purple"},
+      {id:19, text:"Company ", start_date:"25-01-2015", duration:7,order:10, open: false, color:"navy"},
+      {id:20, text:"Task ",    start_date:"25-01-2016", duration:2, order:10, parent:19, color:"navy"},
+      {id:21, text:"Task ",    start_date:"28-01-2016", duration:5, order:10, parent:19, color:"navy"}
+    ]};
+
+    $scope.oldtasks = {
+    links:[
+      { id:1, source:1, target:2, type:"1"},
+      { id:2, source:2, target:3, type:"0"},
+      { id:3, source:3, target:4, type:"0"},
+      { id:4, source:2, target:5, type:"2"},
+    ]};
+
+});
---
+++ js/main.js
@@ -0,0 +1,171 @@
+		gantt.config.show_grid = false;
+
+		var today = new Date();
+		var startOfWeek = getMonday(new Date());
+		var endOfWeek = new Date(startOfWeek.setDate(startOfWeek.getDate()+7));
+
+		var listOfStartDates = [];
+		var listOfEndDates = [];
+		
+
+		var zoom = true;
+		var toggle = false;
+
+		init();
+
+		function left() {
+			startOfWeek.setDate(startOfWeek.getDate()-7);
+			endOfWeek.setDate(endOfWeek.getDate()-7);
+
+			gantt.config.start_date = startOfWeek;
+			gantt.config.end_date = endOfWeek;
+
+			 gantt.render();
+		};
+
+		function right() {
+			startOfWeek.setDate(startOfWeek.getDate()+7);
+			endOfWeek.setDate(endOfWeek.getDate()+7);
+
+			gantt.config.start_date = startOfWeek;
+			gantt.config.end_date = endOfWeek;
+
+			 gantt.render();
+		};
+
+		function toggleTimeFrame() {
+
+			if(zoom) {
+				zoom = false;
+				startOfWeek.setDate(startOfWeek.getDate()-7);
+				endOfWeek.setDate(endOfWeek.getDate()+7);
+				gantt.config.step = 2;
+			} else {
+				zoom = true;
+				startOfWeek.setDate(startOfWeek.getDate()+7);
+				endOfWeek.setDate(endOfWeek.getDate()-7);
+				gantt.config.step = 1;
+			}
+
+			gantt.config.start_date = startOfWeek;
+			gantt.config.end_date = endOfWeek;
+			gantt.render();
+		};
+
+		function mediaQuery() {
+			if(window.innerWidth < 800) {
+				gantt.config.step = 2;
+			}
+		};
+
+		function getMonday(d) {
+		  d = new Date(d);
+		  var day = d.getDay(),
+		      diff = d.getDate() - day + (day == 0 ? -6:1); // adjust when day is sunday
+		  return new Date(d.setDate(diff));
+		};
+
+		function init() {
+			
+			startOfWeek = getMonday(new Date());
+			gantt.config.start_date = startOfWeek;
+			gantt.config.end_date = endOfWeek;
+			mediaQuery();
+		};
+
+		gantt.templates.scale_cell_class = function(date){
+        	if(date.getDay()==0||date.getDay()==6){ return "weekend"; }
+        	if(date.toDateString()==today.toDateString()){ return "today"; }
+    	};
+
+    	gantt.templates.task_class = function(start, end, task){
+    		 
+        	 return task.color;
+    	};
+
+    	gantt.attachEvent("onTaskDblClick", function(id,e){
+		    
+		    if( gantt.getTask(id).open) { 
+		    	gantt.close(id)
+		    } else {
+				gantt.open(id);
+			}
+			return false;
+		});
+
+		gantt.attachEvent("onTaskClick", function(id,e){
+		    
+		    if( gantt.getTask(id).open) { 
+		    	gantt.close(id)
+		    } else {
+				gantt.open(id);
+			}
+			return false;
+		});
+
+		function collapse() {
+
+			if(toggle) {
+				gantt.eachTask(function(task){
+				    gantt.open(task.id)
+				});
+				gantt.render();
+				toggle = false;
+			} else {
+
+				gantt.eachTask(function(task){
+
+				    gantt.close(task.id)
+				});
+				gantt.render();
+				toggle = true;
+			}
+		}
+
+		function setProjects() {
+			gantt.eachTask(function(task){
+				if(task.parent != 0) {			
+					var calc = gantt.calculateEndDate(task.end_date, task.duration);
+
+					if(listOfStartDates[task.parent]<task.start_date || listOfStartDates[task.parent] == null) { listOfStartDates[task.parent]=task.start_date;}
+
+					if(listOfEndDates[task.parent]>calc || listOfEndDates[task.parent] == null) { 
+						
+						listOfEndDates[task.parent]=calc;
+					}
+				}
+			});
+			gantt.eachTask(function(task){
+				if(task.parent == 0) {			
+					task.start_date = listOfStartDates[task.id];
+					task.duration = gantt.calculateDuration(listOfStartDates[task.id], listOfEndDates[task.id]);
+					task.end_date = listOfEndDates[task.id];
+					console.log(task.end_date);	
+				}
+			});
+			gantt.render();
+		}
+
+		gantt.attachEvent("onTaskDrag", function(id, mode, task, original){
+		    
+		});
+
+		gantt.attachEvent("onTaskCreated", function(task){
+			
+		 var level = gantt.calculateTaskLevel(task),
+		   types = gantt.config.types;
+		 
+		 //assign task type based on task level
+		 switch (level){
+		  case 0:
+		   task.type = types.project;
+		   break;
+		  case 1:
+		   task.type = types.subproject;
+		   break;
+		  default:
+		   task.type = types.task;
+		   break;
+		 }
+		 return true;
+		});
